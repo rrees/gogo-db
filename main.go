2343dcf81dabfcb59e4f77901fc640b5a50ed35c
Robert Rees <shudderfix@gmail.com>
2022-12-28 14:15:20 +0000

    Renames the column to match GORM's expectations

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello world")
+
+	dbString := os.Getenv("DB_STRING")
+
+	if dbString == "" {
+		panic("The connection string DB_STRING is not defined")
+	}
+
+	db, err := gorm.Open("mysql", dbString)
+	if err != nil {
+		panic(fmt.Sprintf("failed to connect database: %v", err))
+	}
+	defer db.Close()
 }
